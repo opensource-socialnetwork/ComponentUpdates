9f598d3cf9ddc092d0561c3650fb57754857f14e
Muh Arief Ikhsan Yafi <52633570+muhariefikhsanyafi@users.noreply.github.com>
2024-01-17 22:55:00 +0800

    Update ossn.id.php

--- locale/ossn.id.php
+++ locale/ossn.id.php
@@ -6,9 +6,9 @@
 
 ossn_register_languages('id', array(
 	 'component:update:checker' => 'Memeriksa Update Komponen', 
-	 'component:update:checking' => 'Sedang memeriksa update terbaru. Silakan tunggu...', 
+	 'component:update:checking' => 'Sedang memeriksa update terbaru. Silahkan tunggu...', 
 	 'component:update:update:error' => 'Gagal memperbarui komponen.', 
 	 'component:update:update_available' => 'Update Baru Tersedia', 
-	 'component:update:download' => 'Download', 
+	 'component:update:download' => 'Unduh', 
 	 'component:update:removeserver:error' => 'Tidak dapat memuat data dari server pusat.', 
 ));
